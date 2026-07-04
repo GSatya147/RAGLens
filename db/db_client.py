5f2bacc3c54c325031ddb50e914980a9e4c921dd
GSatya147 <gvvsn147@gmail.com>
2026-07-04 21:33:26 +0100

    code: add postgres client connection skeleton

---
+++ db/db_client.py
@@ -0,0 +1,25 @@
+
+
+def get_connection():
+    pass
+
+def db_connection():
+    pass
+
+def execute_query():
+    pass
+
+def execute_write():
+    pass
+
+def execute_batch():
+    pass
+
+def init_db():
+    pass
+
+def get_db():
+    pass
+
+def clear_db():
+    pass
