92c985375213dce040de3a86b2a0ef9d514ae5c3
GSatya147 <gvvsn147@gmail.com>
2026-07-04 21:51:02 +0100

    code: add persistent jsonl sample data

--- configs/config.py
+++ configs/config.py
@@ -1,1 +1,2 @@
-DATA_FILE_PATH = "./data/musique_ans_v1.0_dev.jsonl"
+DATA_FILE_PATH      = "./data/musique_ans_v1.0_dev.jsonl"
+OUPUT_SAMPLE_DATA   = "./data/musique_270.jsonl"
--- ingestion/sampling.py
+++ ingestion/sampling.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from pandas import DataFrame
 
-from configs.config import DATA_FILE_PATH
+from configs.config import DATA_FILE_PATH, OUPUT_SAMPLE_DATA
 
 def data_loader(file_path: str) -> DataFrame:
-    data = pd.read_json(file_path, lines=True)
+    data = pd.read_json(file_path, lines=True, encoding="utf-8")
     return data
 
 def data_sanity_check(data: DataFrame) -> None:
@@ -13,13 +13,18 @@ def data_sanity_check(data: DataFrame) -> None:
     print(data.iloc[0])
 
 def data_sampler(data: DataFrame) -> DataFrame:
-    
     # extract hop number from "2hop"...
     data["hop_count"] = data["id"].str.extract(r"^(\d+)hop").astype(int)
 
     grouped = data.groupby("hop_count")
 
     sampled = grouped.sample(n=90, random_state=42)
+
+    try:
+        sampled.to_json(OUPUT_SAMPLE_DATA, orient="records", lines=True, force_ascii=False)
+    except Exception as e:
+        print(e)
+
     return sampled
 
 def sample_sanity_check(sample: DataFrame) -> None:
